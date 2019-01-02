4c3a76122470efadc47e43ee6d714eb7d8c028cf
Eric Zhang <ezhang887@gmail.com>
2019-01-01 20:28:36 -0800

    add comments + cleanup some code

--- model.py
+++ model.py
@@ -11,21 +11,27 @@ class Model:
 
     VGG19_LAYERS = [
         'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
-
         'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
-
         'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
-
         'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3', 'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
-
         'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3', 'relu5_3', 'conv5_4', 'relu5_4'
     ]
 
+    """
+    Arguments: 
+    path -- string of the path to the weights file
+    """
     def __init__(self, path):
         print("Loading weights...")
         self.weights = scipy.io.loadmat(path)["layers"][0]
         print("Done loading weights.")
 
+    """
+    Fetches the weights of the specified layer
+
+    Arguments:
+    layer -- name of the layer that we are getting the weights for, see the VGG19_LAYERS list
+    """
     def get_weights(self, layer):
         idx = self.VGG19_LAYERS.index(layer)
         weights, bias = self.weights[idx][0][0][2][0]
@@ -33,16 +39,39 @@ class Model:
         bias = bias.reshape(-1)
         return weights, bias
 
+    """
+    Creates a 2d convolution layer, with the specs of the VGG19 network
+
+    Arguments:
+    input -- a Tensor representing the output of the previous layer
+    layer -- name of the current layer of the network
+    """
     def conv_2d(self, input, layer):
         weights, bias = self.get_weights(layer)
         return tf.nn.bias_add(tf.nn.conv2d(input, tf.constant(weights), strides = (1,1,1,1), padding = "SAME"), bias)
 
+    """
+    Creates a relu activation layer
+
+    Arguments:
+    input -- a Tensor representing the input to the relu activation function
+    """
     def relu(self, input):
         return tf.nn.relu(input)
     
+    """
+    Creates a max pooling layer, with the specs of the VGG19 network
+
+    Arguments:
+    input -- a Tensor representing the input to the max pooling function
+    """
     def max_pool(self, input):
         return tf.nn.max_pool(input, ksize = [1,2,2,1], strides = [1,2,2,1], padding = "SAME")
 
+    """
+    Creates a model with the VGG19 layers
+    Returns the model as a dictionary, with the keys in the dictionary being the names in the VGG19_LAYERS list above
+    """
     def generate_model(self):
         model = {}
         model['input'] = tf.Variable(np.zeros((1, 224, 224, 3)), dtype = 'float32')
@@ -58,6 +87,14 @@ class Model:
             prev_layer = layer
         return model
 
+"""
+Downloads the weights file, given a url, and saves it to the specified path and filename
+
+Arguments:
+url -- a string representing the url of the download link
+filename -- a string representing the name to save the file to
+path -- a string representing the path of where to save the file to, default is the current directory
+"""
 def download_weights(url, filename, path = "./"):
     if not os.path.isfile(filename): 
         try:
--- nst.py
+++ nst.py
@@ -7,12 +7,12 @@ class NeuralStyleTransfer:
             download_weights(WEIGHTS_URL, weights_path, weights_path)
         path = os.path.join(weights_path, weights_filename)
         self.model = Model(path).generate_model()
-        print(self.model)
 
     def run(self):
         pass
 
     def content_cost(self, content_img, other_img):
-        print(content_img.shape)
+        pass
+        
 
 test = NeuralStyleTransfer(WEIGHTS_PATH)
